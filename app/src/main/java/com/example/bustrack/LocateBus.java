436fbbea0230837062635eab18cc69b106e1f4cc
Sahir404 <sahirshah404@gmail.com>
2022-03-23 12:20:19 +0500

    first commit

---
+++ app/src/main/java/com/example/bustrack/LocateBus.java
@@ -0,0 +1,15 @@
+package com.example.bustrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocateBus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locate_bus);
+
+    }
+}
